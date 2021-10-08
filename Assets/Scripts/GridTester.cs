3d403cd40725a4f29c45f6a1c8107294d6b86777
Théo <theogimenez7@gmail.com>
2021-10-08 19:48:10 -0400

    grid system working

--- Assets/Scripts/GridTester.cs
+++ Assets/Scripts/GridTester.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grids : MonoBehaviour
-{
+public class GridTester : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    private void Start () {
+        Grid grid = new Grid (4, 2);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 }
