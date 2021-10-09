a50a2385faba557a7e184a1db40d62e642bfd73b
DomPouleto <theogimenez7@gmail.com>
2021-10-09 11:04:35 -0400

    [FIX] Grids drawing are now currently working

--- Assets/Scripts/CableCreator.cs
+++ Assets/Scripts/CableCreator.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Utils;
 
-public class CableCreator : MonoBehaviour {
+public class CableCreator : MonoBehaviour
+{
     private Vector2 mousePos;
 
     public GameObject finalObject;
@@ -13,23 +15,20 @@ public class CableCreator : MonoBehaviour {
     [SerializeField] private int width;
     [SerializeField] private LayerMask allTilesLayer;
 
-    void Start () {
-        grid = new Grid<GameObject> (width, height, 3f, new Vector3 (0, 0, 0), true);
+    void Start()
+    {
+        grid = new Grid<GameObject>(width, height, 1.5f, new Vector3(0, 0, 0), true);
     }
 
     // Update is called once per frame
-    void Update () {
-        mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-        transform.position = new Vector2 (Mathf.Round (mousePos.x), Mathf.Round (mousePos.y));
-        if (Input.GetMouseButton (0)) {
-            RaycastHit2D rayHit = Physics2D.Raycast (transform.position, Vector2.zero, Mathf.Infinity, allTilesLayer);
-
-            if (rayHit.collider == null) {
-                if (mousePos.x >= grid.GetOriginPosition ().x && mousePos.y >= grid.GetOriginPosition ().y && mousePos.x < grid.GetWidth () && mousePos.y < grid.GetHeight ()) {
-                    Instantiate (finalObject, grid.GetGridPosition (mousePos), Quaternion.identity);
-                } else {
-                    Debug.LogError ("INDEX OUT OF GRID ARRAY, U RETARDED (set value)");
-                }
+    void Update()
+    {
+        mousePos = GetMouseWorldPosition();
+        if (Input.GetMouseButton(0) && grid.IsInGrid(mousePos))
+        {
+            if (grid.GetValue(mousePos) == null)
+            {
+                grid.SetValue(mousePos, Instantiate(finalObject, grid.GetGridPosition(mousePos), Quaternion.identity));
             }
         }
     }
