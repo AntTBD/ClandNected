0b95134c0649f93c6bd04fb4f4781cf47a1bba3e
PAYET Guillaume <payetguillaume736@gmail.com>
2021-10-09 20:54:55 +0200

    Dev/house (#23)
    * [NEW] Add HouseManager (To be completed)
    
    :exclamation: TODO : CreateNewData()
    
    * [NEW] Add DatacenterController
    
    :exclamation: TODO :
     - Traitement d'une donnée TODO
     - Ajouter une nouvelle list TO COMPLET (quand list pleine)
    
    * Correct names
    
    Co-authored-by: AntTBD <antbd@free.fr>

---
+++ Assets/Scripts/HouseController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+Maison :
+ - la maison crée des data pour les envoyer vers un datacenter                                              // TODO
+ - une maison peut être ou non satisfaite de sa connexion aux dataCenters ( décidé par les data )
+ - les maisons sont générées aléatoirement autour des datacenters                                           // In spawn generator
+ - une maison a besoin d'être connectée à un câble pour envoyer des données                                 // if OK
+
+Attributs du MaisonController :
+ - booléen de satisfaction                                                                                  // OK
+ - ref portion de câble                                                                                     // OK
+ - DeltaTime pour l'envoi de données                                                                        // Added
+ - Visiblement c'est tout ?..
+
+Methods :
+ - Génération des data                                                                                      // TODO
+ - Get/Set satisfaction                                                                                     // Added
+ - Set Connected Cable                                                                                      // Added
+
+Note :
+ - Penser à lister les maisons dans un seul GameObject pour calculer la satisfaction générale
+*/
+
+public class HouseController : MonoBehaviour
+{
+    private bool isSatisfied;
+    private CableController connectedCable; // TODO : add Cable script
+
+    [SerializeField] private float sendDeltaTimeSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSatisfied = false;
+        connectedCable = null;
+    }
+
+    public void SetIsSatified(bool etat)
+    {
+        isSatisfied = etat;
+    }
+    public bool IsSatified()
+    {
+        return isSatisfied;
+    }
+    public void ConnectTo(CableController cable)
+    {
+        connectedCable = cable;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SendDatas();
+    }
+
+    IEnumerator SendDatas()
+    {
+        if(connectedCable != null) // une maison a besoin d'être connectée à un câble pour envoyer des données
+        {
+            CreateNewData();
+        }
+
+        yield return new WaitForSeconds(sendDeltaTimeSeconds);
+    }
+
+    /// <summary>
+    /// La maison crée des data pour les envoyer vers un datacenter
+    /// </summary>
+    void CreateNewData()
+    {
+        /// TODO : Create data prefab
+        /// Send data
+    }
+}
