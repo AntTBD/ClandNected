808447ad2d12c1e6ca7d2337de806e169e58d732
Théo <theogimenez7@gmail.com>
2021-10-08 22:47:08 -0400

    Adding the grid system

--- Assets/Scripts/GridTester.cs
+++ Assets/Scripts/GridTester.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridTester : MonoBehaviour {
+    private Grid grid;
     // Start is called before the first frame update
     private void Start () {
-        Grid grid = new Grid (4, 2);
+        grid = new Grid (4, 2, 2f, new Vector3 (0, 0, 0));
     }
 
     // Update is called once per frame
     void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            grid.SetValue (GetMouseWorldPosition (), 56);
+        }
 
+        if (Input.GetMouseButtonDown (1)) {
+            Debug.Log (grid.GetValue (GetMouseWorldPosition ()));
+        }
     }
+
+    // Get Mouse Position in World with Z = 0f
+    public static Vector3 GetMouseWorldPosition () {
+        Vector3 vec = GetMouseWorldPositionWithZ (Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ () {
+        return GetMouseWorldPositionWithZ (Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ (Camera worldCamera) {
+        return GetMouseWorldPositionWithZ (Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ (Vector3 screenPosition, Camera worldCamera) {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint (screenPosition);
+        return worldPosition;
+    }
+
 }
