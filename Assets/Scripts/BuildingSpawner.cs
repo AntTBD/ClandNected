a578e0b098ce59eb8d9cfeec5a16c7883b52d486
nicolasvedrenne <47521326+nicolasvedrenne@users.noreply.github.com>
2021-10-09 01:37:15 -0400

    SpawnHouse
    Need testing
    Need Datacenter class

--- Assets/Scripts/BuildingSpawner.cs
+++ Assets/Scripts/BuildingSpawner.cs
@@ -6,19 +6,25 @@ public class BuildingSpawner : MonoBehaviour
     [SerializeField] private Transform dataCenters;
     [SerializeField] private GridManager gridManager;
     [SerializeField] private int minDistanceBetweenDataCenters;
-    
+    Grid<GameObject> grid;
 
+    void Start() 
+    {
+        grid = gridManager.GetGrid();
+    }
     void SpawnDatacenter()
     {
-        Grid<GameObject> grid = gridManager.GetGrid();
+        
         for (int i = 0; i < 1000; i++)
         {
             Vector3 pos = new Vector3(
                 Random.Range(0, grid.GetWidth() - 1),
                 Random.Range(0, grid.GetHeight() - 1),
                 0);
+            Debug.Log("Random pos : " + pos);
             if (grid.GetValue(pos) == null && !IsTooCloseFromDatacenters(pos))
             {
+                Debug.Log("Pos is valid");
                 //instantiate new datacenter at pos
                 //grid.SetValue(pos, new DataCenter());
             }
@@ -36,5 +42,20 @@ public class BuildingSpawner : MonoBehaviour
             }
         }
         return false;
+    }
+
+    public void SpawnHouse()
+    {
+        var datacenter = dataCenters.GetChild(Random.Range(0, dataCenters.childCount - 1)).position;
+        Vector3 pos = new Vector3(0,0,0);
+        pos.x = RandomFromDistribution.RandomNormalDistribution(datacenter.x, 0.8f);
+        pos.y = RandomFromDistribution.RandomNormalDistribution(datacenter.y, 0.8f);
+        Debug.Log("House pos : " + pos);
+        if (grid.GetValue(pos) == null)
+        {
+            Debug.Log("Pos is valid");
+            //instantiate new house at pos
+            //grid.SetValue(pos, new House());
+        }
     }
 }
