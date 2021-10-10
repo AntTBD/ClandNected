16420f33a8d60b93150ed881b93127789fca45bf
DomPouleto <theogimenez7@gmail.com>
2021-10-09 23:26:10 -0400

    [FIX] UI de ses morts et right click pour le movement

--- Assets/Scripts/CameraMovements.cs
+++ Assets/Scripts/CameraMovements.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovements : MonoBehaviour {
+public class CameraMovements : MonoBehaviour
+{
     public float dragSpeed = 2;
     private Vector3 dragOrigin;
     private Camera camera;
@@ -15,39 +16,44 @@ public class CameraMovements : MonoBehaviour {
     private const int MINSIZE = 4;
     private const int MAXSIZE = 13;
 
-    private void Start () {
-        this.camera = this.gameObject.GetComponent<Camera> ();
+    private void Start()
+    {
+        this.camera = this.gameObject.GetComponent<Camera>();
     }
 
-    void Update () {
+    void Update()
+    {
 
-        if (Input.GetMouseButtonDown (0)) {
+        if (Input.GetMouseButtonDown(1))
+        {
             dragOrigin = Input.mousePosition;
             return;
         }
-        size += Input.GetAxis ("Mouse ScrollWheel");
-        if (size < MINSIZE) {
+        size += Input.GetAxis("Mouse ScrollWheel");
+        if (size < MINSIZE)
+        {
             size = MINSIZE;
         }
-        if (size > MAXSIZE) {
+        if (size > MAXSIZE)
+        {
             size = MAXSIZE;
-            transform.position = new Vector3 (0, 0, -10);
+            transform.position = new Vector3(0, 0, -10);
         }
 
         this.camera.orthographicSize = size;
 
-        if (!Input.GetMouseButton (0)) return;
+        if (!Input.GetMouseButton(1)) return;
 
-        Vector3 pos = Camera.main.ScreenToViewportPoint (Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3 (pos.x * dragSpeed, pos.y * dragSpeed, 0);
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
 
-        transform.Translate (move, Space.World);
-        int width = gridManager.GetGrid ().GetWidth ();
-        int height = gridManager.GetGrid ().GetHeight ();
-        Vector3 originPosition = gridManager.GetGrid ().GetOriginPosition ();
-        float x = Mathf.Clamp (transform.position.x, originPosition.x, originPosition.x + width);
-        float y = Mathf.Clamp (transform.position.y, originPosition.y, originPosition.y + height);
-        transform.position = new Vector3 (x, y, -10);
+        transform.Translate(move, Space.World);
+        int width = gridManager.GetGrid().GetWidth();
+        int height = gridManager.GetGrid().GetHeight();
+        Vector3 originPosition = gridManager.GetGrid().GetOriginPosition();
+        float x = Mathf.Clamp(transform.position.x, originPosition.x, originPosition.x + width);
+        float y = Mathf.Clamp(transform.position.y, originPosition.y, originPosition.y + height);
+        transform.position = new Vector3(x, y, -10);
 
     }
 }
